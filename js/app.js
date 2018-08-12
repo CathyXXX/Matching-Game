e73ac372602f28585febbce097fe74980adb5c7f
CathyXXX <40568351+CathyXXX@users.noreply.github.com>
2018-08-12 11:58:40 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -21,13 +21,6 @@ function shuffleDeck() {
 }
 shuffleDeck();
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length,
@@ -41,21 +34,8 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
 
 //listening for card clicks and flipping them over with toggle 
 deck.addEventListener('click', event => {
@@ -77,10 +57,8 @@ deck.addEventListener('click', event => {
 
 function isValid(clickedCard) {
     return (
-        clickedCard.classList.contains('card') &&
-        !clickedCard.classList.contains('match') &&
-        flipCards.length < 2 &&
-        !flipCards.includes(clickedCard)
+        clickedCard.classList.contains('card') && !clickedCard.classList.contains('match')
+        && flipCards.length < 2 && !flipCards.includes(clickedCard)
     );
 }
 
